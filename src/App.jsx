9334bb15004c6d4f69313f074a4dd42c4f97f9bd
yamileth-quispe <jhoquispeespinoza@crackthecode.la>
2026-02-16 15:28:15 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Hola 👋</h1>
+      <p>Si ves esto, React está funcionando correctamente.</p>
+    </div>
+  );
+}
+
+export default App;
