59ba92c6aa2d24638934bca2c32ed5e2934e518a
yamileth-quispe <jhoquispeespinoza@crackthecode.la>
2026-02-16 16:06:14 -0500

    build final para github pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/pagina-web/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   plugins: [react(), tailwindcss()],
 })
